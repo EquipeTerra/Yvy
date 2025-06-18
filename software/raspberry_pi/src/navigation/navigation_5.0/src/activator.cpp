ab51dc6e744aedf2733974779b20d6764be3e97b
2005HAK <hebertkubis15@gmail.com>
2025-06-18 17:15:43 -0300

    Correção de erros 9

--- software/raspberry_pi/src/navigation/navigation_5.0/src/activator.cpp
+++ software/raspberry_pi/src/navigation/navigation_5.0/src/activator.cpp
@@ -9,6 +9,7 @@ void Activator::WaitingForActivation() {
     // Esse pino precisa de um resistor de pull-up
     int value = gpioRead(pinNumber);
     while(value){
+        int value = gpioRead(pinNumber);
         cout << "Waiting for activation: " << value << endl;
         sleep_for(milliseconds(100));
     }
